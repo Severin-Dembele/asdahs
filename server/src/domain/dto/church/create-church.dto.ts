ce1ae74da934990b4ff942dcde13dc4b5589357e
BONOU <eudesbonou@gmail.com>
2024-05-27 22:08:42 +0000

    type_church

--- server/src/domain/dto/church/create-church.dto.ts
+++ server/src/domain/dto/church/create-church.dto.ts
@@ -7,4 +7,6 @@ export class CreateChurchDto {
   city: string;
   @ApiProperty()
   country: string;
+  @ApiProperty()
+  typeChurch: string;
 }
--- server/src/repositories/church.service.ts
+++ server/src/repositories/church.service.ts
@@ -14,6 +14,7 @@ export class ChurchService {
         country: createChurchDto.country,
         name: createChurchDto.name,
         conferenceId: conferenceId,
+        typeChurch: createChurchDto.typeChurch
       },
     });
   }
